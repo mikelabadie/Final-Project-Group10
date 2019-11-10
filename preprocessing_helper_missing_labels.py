4c75f38950b1d46b44329d42c9b399a85c1d024f
Mike Labadie <43349635+mikelabadie@users.noreply.github.com>
2019-11-10 15:09:05 -0500

    Add files via upload

--- preprocessing_helper_missing_labels.py
+++ preprocessing_helper_missing_labels.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from PIL import Image
 from project_configuration import image_directory
 from shutil import copyfile
 
+
+#%% create a csv with sequences that are not tagged
+# we used this csv to perform manual tagging on the peak image for 266 sequences
+
 df = pd.read_csv("labels.csv")
 
+# we first copied the peak image for all unlabeled sequences to a single folder for easy tagging
 count=0
 for _, row in df[df["class"].isna()].iterrows():
     new_filename="/home/ubuntu/Project/NoLabel/"+row["peak_image_filename"].split("/")[-1]
     copyfile(row["peak_image_filename"],new_filename)
     count+=1
 
-
-#%%
+# we created a csv that we used to manually code the emotion tag
 subjects = []
 labels_by_subject = []
 missing_labels_by_subject = []
@@ -28,11 +31,16 @@ for subj in df["subject_name"].drop_duplicates():
     missing_labels_by_subject.append(",".join(list(possible_labels-already_tagged)))
 
 df2 = pd.DataFrame(subjects,columns=["subject_name"])
-#df2["already_tagged"]=labels_by_subject
 df2["missing_tags"]=missing_labels_by_subject
 
+df2 = pd.merge(df,df2,how="left")
+df2 = df2[df2["class"].isna()]
+df2.to_csv("missing_labels.csv",index=False)
 
-#%%
-df3 = pd.merge(df,df2,how="left")
-df3 = df3[df3["class"].isna()]
-df3.to_csv("missing_labels.csv",index=False)
+
+#%% merge the given labels and our manual labels into a single csv
+df = pd.read_csv("labels.csv")
+df2 = pd.read_csv("missing_labels_completed.csv")
+df = pd.merge(df,df2[["peak_image_filename","class"]], how="left",on="peak_image_filename")
+df.columns = ['subject_name', 'set_name', 'peak_image_filename', 'class','class_manual']
+df.to_csv("labels_all.csv",index=False)
