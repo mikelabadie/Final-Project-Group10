6a55292361530caded5eb41376f5b7ee04097df6
Mike Labadie <43349635+mikelabadie@users.noreply.github.com>
2019-11-10 14:39:43 -0500

    Add files via upload

--- preprocessing_helper_missing_labels.py
+++ preprocessing_helper_missing_labels.py
@@ -12,3 +12,27 @@ for _, row in df[df["class"].isna()].iterrows():
     new_filename="/home/ubuntu/Project/NoLabel/"+row["peak_image_filename"].split("/")[-1]
     copyfile(row["peak_image_filename"],new_filename)
     count+=1
+
+
+#%%
+subjects = []
+labels_by_subject = []
+missing_labels_by_subject = []
+
+possible_labels = set(df[~(df["class"].isna())]["class"].drop_duplicates())
+
+for subj in df["subject_name"].drop_duplicates():
+    already_tagged = set(df[(df["subject_name"]==subj)&~(df["class"].isna())]["class"].values)
+    subjects.append(subj)
+    labels_by_subject.append(",".join(list(already_tagged)))
+    missing_labels_by_subject.append(",".join(list(possible_labels-already_tagged)))
+
+df2 = pd.DataFrame(subjects,columns=["subject_name"])
+#df2["already_tagged"]=labels_by_subject
+df2["missing_tags"]=missing_labels_by_subject
+
+
+#%%
+df3 = pd.merge(df,df2,how="left")
+df3 = df3[df3["class"].isna()]
+df3.to_csv("missing_labels.csv",index=False)
