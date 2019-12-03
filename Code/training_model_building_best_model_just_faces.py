cd7343f98553fba01ba54152acd75477d5745dad
mwilchek <mwilchek@gmu.edu>
2019-12-02 23:11:29 -0500

    adding model and updated format

--- Code/training_model_building_best_model_just_faces.py
+++ Code/training_model_building_best_model_just_faces.py
@@ -1,4 +1,4 @@
-#%% --------------------------------------- Imports --------------------------------------------------------------------
+# %% --------------------------------------- Imports --------------------------------------------------------------------
 from PIL import Image
 import pandas as pd
 import numpy as np
@@ -18,8 +18,7 @@ from keras_preprocessing.image import ImageDataGenerator
 from configuration import training_images_list_filename, training_images_list_filename_just_faces, \
     validation_images_list_filename, validation_images_list_filename_just_faces, model_filename_just_faces
 
-
-#%% ---------------------------------------- Set-Up --------------------------------------------------------------------
+# %% ---------------------------------------- Set-Up --------------------------------------------------------------------
 SEED = 42
 os.environ['PYTHONHASHSEED'] = str(SEED)
 random.seed(SEED)
@@ -27,20 +26,20 @@ np.random.seed(SEED)
 tf.random.set_seed(SEED)
 weight_init = glorot_uniform(seed=SEED)
 
-image_size = (54,72)
+image_size = (54, 72)
 
-#%%
+# %%
 train_df = pd.read_csv(training_images_list_filename_just_faces)
 
-datagen=ImageDataGenerator(rescale=1./255.,
-                           validation_split=0.25,
-                           horizontal_flip=True
-                           #,height_shift_range=1
-                           #,width_shift_range=1
-                           #,rotation_range=1
-                           )
+datagen = ImageDataGenerator(rescale=1. / 255.,
+                             validation_split=0.25,
+                             horizontal_flip=True
+                             # ,height_shift_range=1
+                             # ,width_shift_range=1
+                             # ,rotation_range=1
+                             )
 
-train_generator=datagen.flow_from_dataframe(
+train_generator = datagen.flow_from_dataframe(
     dataframe=train_df,
     directory=None,
     x_col="name",
@@ -52,7 +51,7 @@ train_generator=datagen.flow_from_dataframe(
     class_mode="categorical",
     target_size=image_size)
 
-valid_generator=datagen.flow_from_dataframe(
+valid_generator = datagen.flow_from_dataframe(
     dataframe=train_df,
     directory=None,
     x_col="name",
@@ -64,10 +63,9 @@ valid_generator=datagen.flow_from_dataframe(
     class_mode="categorical",
     target_size=image_size)
 
-
-#%%
+# %%
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(image_size[0],image_size[1],3), padding='same'))
+model.add(Conv2D(32, (3, 3), input_shape=(image_size[0], image_size[1], 3), padding='same'))
 model.add(Activation('elu'))
 
 model.add(Conv2D(32, (3, 3)))
@@ -89,15 +87,16 @@ model.add(Activation('elu'))
 model.add(Dropout(0.5))
 model.add(Dense(7, activation='softmax'))
 
-model.compile(Adam(lr=0.001, decay=1e-6),loss="categorical_crossentropy",metrics=["accuracy"])
+model.compile(Adam(lr=0.001, decay=1e-6), loss="categorical_crossentropy", metrics=["accuracy"])
 
 # checkpoints
-checkpoint = ModelCheckpoint(model_filename_just_faces, monitor='val_accuracy', save_best_only=True, mode='max', verbose=1)
-#checkpoint = ModelCheckpoint(model_filename, monitor='val_loss', save_best_only=True, mode='min', verbose=1)
+checkpoint = ModelCheckpoint(model_filename_just_faces, monitor='val_accuracy', save_best_only=True, mode='max',
+                             verbose=1)
+# checkpoint = ModelCheckpoint(model_filename, monitor='val_loss', save_best_only=True, mode='min', verbose=1)
 callbacks_list = [checkpoint]
 
-STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
-STEP_SIZE_VALID=valid_generator.n//valid_generator.batch_size
+STEP_SIZE_TRAIN = train_generator.n // train_generator.batch_size
+STEP_SIZE_VALID = valid_generator.n // valid_generator.batch_size
 model.fit_generator(generator=train_generator,
                     steps_per_epoch=STEP_SIZE_TRAIN,
                     validation_data=valid_generator,
